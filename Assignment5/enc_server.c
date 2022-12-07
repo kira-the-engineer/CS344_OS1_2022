69d1dcbb721146bc22da6985fb52f26d2ecbc31f
Kira Kopcho <kopchok@oregonstate.edu>
2022-12-07 15:34:58 -0800

    Most of server code written, still need to write encryption func

--- Assignment5/enc_server.c
+++ Assignment5/enc_server.c
@@ -21,12 +21,14 @@ int main(int argc, char *argv[]){
 	listenSock = socket(AF_INET, SOCK_STREAM, 0);
 	if(listenSock < 0) {
 		error("SERVER: ERROR Opening Socket \n");
+		exit(1);
 	}
 
 	//if sock is successfully created, bind to port and begin to listen
 	if(bind(listenSock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
 		close(listenSock);
 		error("SERVER: ERROR on binding \n");
+		exit(1);
 	}
 	listen(listenSock, MAX_CONNS); //allow up to 5 concurrent conns
 
@@ -41,21 +43,71 @@ int main(int argc, char *argv[]){
 			close(listenSock); //shut off listener
 			error("SERVER: ERROR on accept \n");
 			continue; //from Beej's socket tutorial - simple stream server
-		}
+		} //eo if
 
 		//Below code follows Beej's Stream Server example, uses a child process to handle encoding data
 		pid = fork(); //fork that process!
 		if(pid == -1){ //if fork was unsuccessful
 			close(listenSock); //stop listening for incoming conns
-			error("SERVER: ERROR could not fork process");
+			error("SERVER: ERROR could not fork process \n");
 			continue;
-		}
+		} //eo if
 		else{ //if we actually spawned a child properly
-			//create strings for 
+			//create strings for recieved data from client
 			char recvbuf[MAX_BUFFER]; //create buffer for rx'd data
 			char encryptmsg[MAX_BUFFER]; //create string to store encrypted message
 			char plaintxt[MAX_BUFFER]; //store plaintext
 			char keytxt[MAX_BUFFER]; //store key
-		}
-	} 
-}
+
+			//init buffer to 0 with memset
+			memset(recvbuf, '\0', sizeof(recvbuf));
+
+			//read the client's handshake first to ensure we conn to right server
+			while(chars_rd == 0){
+				chars_rd = recv(connectSock, recvbuf, sizeof(recvbuf) - 1, 0); //rx handshake msg
+				if(chars_rd < 0) { //make sure we actually read from connected client properly
+					error("SERVER: ERROR cannot read from client \n");
+					close(connectSock);
+					exit(1);
+				} //eo if
+			} //eo while
+
+			//verify handshake message against defined message
+			if(strcmp(recvbuf, ENC_CONFIRM_MSG) != 0){ //if the wrong client connects by accident
+				printf("SERVER: Client handshake message does not match \n");
+				chars_rd = send(connectSock, BAD_SERV, 7, 0); //send rejection message to client
+				if(chars_rd < 0) {
+					error("SERVER: ERROR cannot write to client \n");
+					close(connectSock);
+					exit(2);
+				} //eo inner if
+				exit(2);
+			} //eo outer if
+			else { //right client connected
+			      printf("SERVER: Connected to correct client \n");
+			      chars_rd = send(connectSock, CORRECT_SERV, 7, 0); //send accept message to client
+			      if(chars_rd < 0) {
+					error("SERVER: ERROR cannot write to client \n");
+					close(connectSock);
+					exit(2);
+			      } //eo if
+			
+			     memset(recvbuf, 0, MAX_BUFFER); //clear buffer
+			     chars_rd = 0; //reset chars read count
+
+			     while(chars_rd == 0) { //read file size
+				    chars_rd = recv(connectSock, recvbuf, sizeof(recvbuf) - 1, 0); //rx file len from client
+				    if(chars_rd < 0){
+					 error("SERVER: ERROR cannot read from client \n");
+				    } //eo if
+			     } //eo while
+			     int flen = atoi(recvbuf); //get file length as an int
+
+			     chars_rd = 0; //reset chars cnt
+			     memset(recvbuf, 0, MAX_BUFFER); //clr buffer
+
+			} //eo else
+
+		} //eo while
+
+} //eo main
--- Assignment5/funcs.h
+++ Assignment5/funcs.h
@@ -16,7 +16,8 @@
 #define MAX_BUFFER 100000 //max plaintext is around 70000 chars, buffer needs to fit it
 #define ENC_CONFIRM_MSG "enc_cli" //message sent from enc_client to enc_server
 #define DEC_CONFIRM_MSG "dec_cli" //message sent from dec_client to dec_server
-
+#define BAD_SERV "reject"
+#define CORRECT_SERV "accept"
 
 //function prototyes
 void setupAddressStruct(struct sockaddr_in*, int); 
